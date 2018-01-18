763b97ad65236d42aac7a12142c360f7b6a1d6bd
bobylegros <rafcapdeville@gmail.com>
2018-01-18 22:39:33 +0100

    fin

---
+++ Vauréal.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+
+#On cré une fonction qui va permettre d'aller récupérer un URL
+def scrapper_for_dummies(url, path)
+	page = Nokogiri::HTML(open(url))
+	return page.css(path)
+end
+
+
+
+def get_the_email_of_a_townhall_from_its_webpage(reste_url)
+	url = $base_url + reste_url                      #On concatène les variable pour n'avoir plus qu'à préciser le path par la suite
+	mail = scrapper_for_dummies(url, "td p font")    #On précise le "path" dans l'URL pour permettre à la variable "mail"
+	mail.each do |mail|                              
+	    if mail.text.include?('@')                   #On précise que l'on récupère le fichier texte de la variable "mail" s'il contient un @
+	        puts mail.text
+	    end
+	end
+end
+
+#Est une variable dites constante . Le $ permet de l'appeler dans une fonction.
+$base_url = "http://annuaire-des-mairies.com/"
+
+#Ici on appel la fonction en précisant la suite de l'URL que l'on veut
+get_the_email_of_a_townhall_from_its_webpage("95/vaureal.html")
